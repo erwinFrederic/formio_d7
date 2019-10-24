e504e0f0b63ae21828cb506e0e4b415348abbfdd
Erwin Sittie <erwin.sittie@gmail.com>
2019-10-24 10:40:47 +0000

    creating the form

---
+++ sites/all/modules/_custom/formio/templates/maint.tpl.php
@@ -0,0 +1,23 @@
+<?php
+drupal_add_js(
+    drupal_get_path('module', 'formio') . '/dist/main.bundle.js'
+);
+?>
+
+<link rel="stylesheet" href="https://unpkg.com/formiojs@4.0.0/dist/formio.full.min.css">
+
+<div id="formio">
+
+    <template>
+
+      <h1>formbuilder</h1>
+      <formbuilder v-bind:form="form" v-on:submit="onSubmit(form)"></formbuilder>
+      <div class="jsonStructure">
+        {{ jsonFormDefinition }}
+      </div>
+      <div class="">
+        {{ renderForm }}
+      </div>
+    </template>
+
+</div>
